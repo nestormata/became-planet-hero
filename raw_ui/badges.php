80b63e3ea254921bc0e366e85e741a3297da869f
Nestor Mata <nestor@profesional.co.cr>
2014-12-06 21:30:06 +0000

    Added UI files from Alvis to be processed and integrated

---
+++ raw_ui/badges.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<?php include 'header.php' ?>
+	<body>
+		<div class="container demo-3">
+            <?php include 'sidebar.php' ?>
+			<!-- Top Navigation -->
+			<header style="padding-top:7em;">
+				<h1>Badges</h1>	
+				<nav class="codrops-demos">
+				</nav>
+			</header>
+			<ul class="grid cs-style-4">
+				<li>
+					<figure>
+						<div><img src="Badges/images/5.png" alt="img05"></div>
+						<figcaption>
+							<h3>Safari</h3>
+							<span>Jacob Cummings</span>
+							<a href="http://dribbble.com/shots/1116775-Safari">Take a look</a>
+						</figcaption>
+					</figure>
+				</li>
+				<li>
+					<figure>
+						<div><img src="Badges/images/6.png" alt="img06"></div>
+						<figcaption>
+							<h3>Game Center</h3>
+							<span>Jacob Cummings</span>
+							<a href="http://dribbble.com/shots/1118904-Game-Center">Take a look</a>
+						</figcaption>
+					</figure>
+				</li>
+				<li>
+					<figure>
+						<div><img src="Badges/images/2.png" alt="img02"></div>
+						<figcaption>
+							<h3>Music</h3>
+							<span>Jacob Cummings</span>
+							<a href="http://dribbble.com/shots/1115960-Music">Take a look</a>
+						</figcaption>
+					</figure>
+				</li>
+				<li>
+					<figure>
+						<div><img src="Badges/images/4.png" alt="img04"></div>
+						<figcaption>
+							<h3>Settings</h3>
+							<span>Jacob Cummings</span>
+							<a href="http://dribbble.com/shots/1116685-Settings">Take a look</a>
+						</figcaption>
+					</figure>
+				</li>
+				<li>
+					<figure>
+						<div><img src="Badges/images/1.png" alt="img01"></div>
+						<figcaption>
+							<h3>Camera</h3>
+							<span>Jacob Cummings</span>
+							<a href="http://dribbble.com/shots/1115632-Camera">Take a look</a>
+						</figcaption>
+					</figure>
+				</li>
+				<li>
+					<figure>
+						<div><img src="Badges/images/3.png" alt="img03"></div>
+						<figcaption>
+							<h3>Phone</h3>
+							<span>Jacob Cummings</span>
+							<a href="http://dribbble.com/shots/1117308-Phone">Take a look</a>
+						</figcaption>
+					</figure>
+				</li>
+			</ul>
+		</div><!-- /container -->
+	</body>
+</html>
