2a82d85a3dd14d81db0607b62fe0059d587574ff
Dmitry Romanenko <dimon222@gmail.com>
2014-12-07 08:40:20 -0500

    User API

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,45 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    connection = require('../helpers/mysql.js'),
+    router = express.Router();
 
-/* GET users listing. */
+// Get full list of teams
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  var query = connection.query('SELECT * from Users',
+    function(err, rows, fields) {
+        if (err) {throw err;}
+        if (!rows.length) {return res.send(404);}
+        res.render('userids', { rows: rows});
+  });
+  console.log(query.sql);
+});
+
+// Get information about team by id
+router.get('/:id', function(req, res) {
+  var query = connection.query('SELECT * from Users WHERE UserID=?', [req.params.id],
+    function(err, rows, fields) {
+        if (err) {throw err;}
+        if (!rows.length) {return res.send(404);}
+        res.render('userids', { rows: rows});
+  });
+
+  console.log("Requested userid - " + req.params.name);
+});
+
+// Delete user
+router.delete('/', function(req, res) {
+  console.log(req.body);
+  var query = connection.query('DELETE FROM Users WHERE UserID=?', [req.param('id')],
+    function(err, result) {
+        if (err) {res.send("Failure"); throw err;}
+        res.send('OK');
+        console.log(result);
+  });
+  console.log(query.sql);
+});
+
+// Update user profile (?) TBD
+router.post('/update', function(req, res) {
+  res.send('Coming soon');
 });
 
 module.exports = router;
