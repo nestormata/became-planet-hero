38e90ce03af657c453af090c523853cfba1d4f07
Nestor Mata <nestor@profesional.co.cr>
2014-12-07 12:29:13 +0000

    Added configuration for staging

---
+++ configuration/config.staging.js
@@ -0,0 +1,10 @@
+module.exports={
+  "facebook_api_key"      :     "1511474802438575",
+  "facebook_api_secret"   :     "e59a7dfa80d03f4c114bd2e5d8d17040",
+  "callback_url"          :     "http://unkk41c5829b.nestormata.koding.io:3000/auth/facebook/callback",
+  "use_database"          :     "true",
+  "host"                  :     "localhost",
+  "username"              :     "hacker",
+  "password"              :     "H@cker1",
+  "database"              :     "hackathon"
+}
